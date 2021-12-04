7d90e42a975895e79d6a3185d288fc8b8716ebcf
ShaoZhilei <shaozl0811@163.com>
2021-12-04 08:36:24 +0800

    gmv commit

---
+++ gmall-realtime/src/main/scala/com/szl/app/GmvApp.scala
@@ -0,0 +1,47 @@
+package com.szl.app
+
+import com.alibaba.fastjson.JSON
+import com.szl.bean.OrderInfo
+import com.szl.constants.GmallConstants
+import com.szl.utils.MyKafkaUtil
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.phoenix.spark._
+
+class GmvApp {
+  def main(args: Array[String]): Unit = {
+    //1.创建SparkConf
+    val sparkConf: SparkConf = new SparkConf().setAppName("GmvApp").setMaster("local[*]")
+
+    //2.创建SparkStreamingContext
+    val ssc: StreamingContext = new StreamingContext(sparkConf,Seconds(5))
+
+    //3.获取Kafka数据
+    val kafkaDStream: InputDStream[ConsumerRecord[String, String]] = MyKafkaUtil.getKafkaDStream(GmallConstants.KAFKA_TOPIC_ORDER,ssc)
+
+    //4.将JSON数据转化为样例类
+    val orderInfoDStream: DStream[OrderInfo] = kafkaDStream.mapPartitions(partition => {
+      partition.map(record => {
+        val orderInfo: OrderInfo = JSON.parseObject(record.value(), classOf[OrderInfo])
+        orderInfo.create_date = orderInfo.create_time.split(" ")(0)
+        orderInfo.create_hour = orderInfo.create_time.split(" ")(1).split(":")(0)
+        orderInfo
+      })
+    })
+    //5.将数据写入Hbase
+    orderInfoDStream.foreachRDD(rdd=>{
+      rdd.saveToPhoenix(
+        "GMALL_SSC_ORDER_INFO",
+        Seq("ID", "PROVINCE_ID", "CONSIGNEE", "ORDER_COMMENT", "CONSIGNEE_TEL", "ORDER_STATUS", "PAYMENT_WAY", "USER_ID", "IMG_URL", "TOTAL_AMOUNT", "EXPIRE_TIME", "DELIVERY_ADDRESS", "CREATE_TIME", "OPERATE_TIME", "TRACKING_NO", "PARENT_ORDER_ID", "OUT_TRADE_NO", "TRADE_BODY", "CREATE_DATE", "CREATE_HOUR"),
+        HBaseConfiguration.create(),
+        Some("hadoop102,hadoop103,hadoop104:2181")
+      )
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
